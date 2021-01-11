dadcd4e762ba3f594af4831f4d67077479a4f022
STUART GRAHAM <stuart@Renees-MacBook-Pro.local>
2021-01-11 19:48:27 +0000

    homework

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,9 +1,22 @@
 # WRITE YOUR FUNCTIONS HERE
+# 
+import pprint
 
-def get_pet_shop_name ():
-    return "Camelot of Pets"
+def get_pet_shop_name (pet_shop):
+    pprint.pprint(pet_shop)
+    return pet_shop["name"] 
 
-def get_total_cash
-return 1000
+def get_total_cash(pet_shop):
+    return pet_shop ["admin"]["total_cash"]
 
-# def  get_total_cash
+def add_or_remove_cash (pet_shop, cash):
+    pet_shop["admin"]["total_cash"] =  pet_shop["admin"]["total_cash"] + cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop['admin']["pets_sold"]
+
+def increase_pets_sold (pet_shop, sold):
+    pet_shop['admin']["pets_sold"] = pet_shop['admin']["pets_sold"] + sold 
+
+def get_stock_count(pet_shop):
+    return len(pet_shop)["pets"]
