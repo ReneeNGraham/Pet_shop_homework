84e79c991a144ecacd4508ee584647981cbb51c2
Renee Graham <renee.noluthando@gmail.com>
2021-02-04 23:08:00 +0000

    Update pet_shop.py

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,33 +1,61 @@
 # WRITE YOUR FUNCTIONS HERE
-# 
-import pprint
 
-def get_pet_shop_name (pet_shop):
-    pprint.pprint(pet_shop)
-    return pet_shop["name"] 
-
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+    
 def get_total_cash(pet_shop):
-    return pet_shop ["admin"]["total_cash"]
+    return pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash (pet_shop, cash):
-    pet_shop["admin"]["total_cash"] =  pet_shop["admin"]["total_cash"] + cash
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
 
 def get_pets_sold(pet_shop):
-    return pet_shop['admin']["pets_sold"]
+    return pet_shop["admin"]["pets_sold"]
 
-def increase_pets_sold (pet_shop, sold):
-    pet_shop['admin']["pets_sold"] = pet_shop['admin']["pets_sold"] + sold 
+def increase_pets_sold(pet_shop, num_pets_sold):
+    pet_shop["admin"]["pets_sold"] += num_pets_sold
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
-    #####This is how far I got on my own. 
-
 def get_pets_by_breed(pet_shop, breed):
     found_pets = []
-    for pet in pet_shop["pets"]:
+    for pet in pet_shop ["pets"]:
         if pet["breed"] == breed:
-            found_pets.append(pet)
+          found_pets.append(pet)
 
     return found_pets
 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop ["pets"]:
+        if pet["name"] == name:
+           return pet
+    
+def remove_pet_by_name(pet_shop, name):
+    pet_shop["pets"].remove(find_pet_by_name(pet_shop, name))
+
+def add_pet_to_stock(pet_shop, pet):
+    pet_shop["pets"].append(pet)
+
+def get_customer_cash(customer):
+   return customer["cash"]
+
+def remove_customer_cash (customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
+def customer_can_afford_pet(customer, pet):
+    return customer["cash"] >= pet["price"]
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet != None and customer_can_afford_pet(customer, pet):
+        remove_pet_by_name(pet_shop, pet ["name"])
+        add_pet_to_customer(customer, pet)
+        remove_customer_cash (customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        increase_pets_sold(pet_shop,1)
